4cd14829d498a068c5f4c5df27581d94a2aa47bc
Ayat Aliev <ayataliev0@gmail.com>
2020-01-28 15:53:57 +0600

    zz

--- app/src/main/java/com/example/weatherapp/data/NotificationHelper.java
+++ app/src/main/java/com/example/weatherapp/data/NotificationHelper.java
@@ -13,57 +13,46 @@ import androidx.core.app.NotificationManagerCompat;
 
 import com.example.weatherapp.R;
 import com.example.weatherapp.ui.main.MainActivity;
-import com.example.weatherapp.ui.main.MyForegroundService;
+import com.example.weatherapp.ui.main.MapActivity;
 
 public class NotificationHelper {
     public static final String NOTIFICATION_CHANEL = "chanel_notification";
-    public static final String ACTION_CLOSE = "action_close";
+    private static final String CHANNEL_ID = "CHANNEL_ID";
 
-    public static Notification getNotification(Context context) {
+
+    public static Notification createNotification(Context context, String title, String body) {
         createNotificationChannel(context);
-//        Intent intent =new Intent(context, MainActivity.class);
-//        PendingIntent pendingIntent = PendingIntent.getActivity(context, 200,intent,0);
-
-
-
+        Intent intent = new Intent(context, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context,1,intent,0);
+        Intent intent1 = new Intent(context, MapActivity.class);
+        PendingIntent pendingIntent1 = PendingIntent.getActivity(context,2,intent1,0);
         return new NotificationCompat.Builder(context, NOTIFICATION_CHANEL)
                 .setSmallIcon(R.mipmap.ic_launcher)
-                .setContentTitle("My ")
-                .setAutoCancel(false)
-                .setContentIntent(createPendingIntent(context))
-                .setContentText("Much longer text that cannot fit one line...")
-                .setStyle(new NotificationCompat.BigTextStyle()
-                        .bigText("Much longer text that cannot fit one line..."))
+                .setContentTitle(title)
+                .setContentText(body)
+                .setContentIntent(pendingIntent)
+                .setVisibility(NotificationCompat.VISIBILITY_PUBLIC)
                 .setPriority(NotificationCompat.PRIORITY_HIGH)
+                .addAction(R.drawable.ic_place,"Second",pendingIntent1)
                 .build();
 
     }
 
-    public static void createNotification(Context context) {
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-        notificationManager.notify(1, getNotification(context));
+    public static void showNotification(Context context, Notification notification){
+        NotificationManagerCompat managerCompat = NotificationManagerCompat.from(context);
+        managerCompat.notify(1,notification );
     }
 
-
-    private static PendingIntent createPendingIntent(Context context) {
-        Intent intent = new Intent(context, MainActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_SINGLE_TOP);
-        return PendingIntent.getActivity(context, 0, intent, 0);
-    }
 
     private static void createNotificationChannel(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            CharSequence name = "test title";
-            String description = "description";
-            int importance = NotificationManager.IMPORTANCE_DEFAULT;
-            NotificationChannel channel = new NotificationChannel(NOTIFICATION_CHANEL, name, importance);
+            CharSequence name = "channel name";
+            String description = "channel desc";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, name, importance);
             channel.setDescription(description);
-            // Register the channel with the system; you can't change the importance
-            // or other notification behaviors after this
             NotificationManager notificationManager = context.getSystemService(NotificationManager.class);
-            if (notificationManager != null) {
-                notificationManager.createNotificationChannel(channel);
-            }
+            notificationManager.createNotificationChannel(channel);
         }
     }
 }
--- app/src/main/java/com/example/weatherapp/ui/main/MyForegroundService.java
+++ app/src/main/java/com/example/weatherapp/ui/main/MyForegroundService.java
@@ -1,17 +1,35 @@
 package com.example.weatherapp.ui.main;
 
+import android.Manifest;
 import android.app.Notification;
 import android.app.Service;
 import android.content.Intent;
+import android.location.Location;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.PermissionChecker;
 
 import com.example.weatherapp.data.NotificationHelper;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+
+import java.util.ArrayList;
+
 import static com.example.weatherapp.ui.main.MainActivity.IS_SERVICE_ACTIVE;
 
 
 public class MyForegroundService extends Service {
+
+
+    private FusedLocationProviderClient fusedLocationProvider;
+    private LocationRequest locationRequest;
+    private ArrayList<Location> locations = new ArrayList<>();
+
     @Nullable
     @Override
     public IBinder onBind(Intent intent) {
@@ -20,10 +38,10 @@ public class MyForegroundService extends Service {
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        if (intent.getBooleanExtra(IS_SERVICE_ACTIVE, false)) {
-            startForeground(1, NotificationHelper.getNotification(getApplicationContext()));
-        } else {
-            stopSelf();
+        startForeground(1, NotificationHelper.createNotification(getApplicationContext()
+                ,"locations","body"));
+        if (intent.getBooleanExtra("true",false) == true){
+            requestLocationUpdates();
         }
 
         return START_STICKY;
@@ -37,7 +55,31 @@ public class MyForegroundService extends Service {
 
     }
 
-    private void coord() {
+    public void requestLocationUpdates() {
+
+        if (ContextCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)==
+                PermissionChecker.PERMISSION_GRANTED &&
+                ContextCompat.checkSelfPermission(getApplicationContext(),Manifest.permission.ACCESS_FINE_LOCATION)==
+                        PermissionChecker.PERMISSION_GRANTED ){
+            fusedLocationProvider = new FusedLocationProviderClient(this);
+            locationRequest = new LocationRequest();
+            locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+            locationRequest.setFastestInterval(2000);
+            locationRequest.setInterval(4000);
+            fusedLocationProvider.requestLocationUpdates(locationRequest, new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+
+                    super.onLocationResult(locationResult);
+                    Log.e("------", "lat: " + locationResult.getLastLocation().getLatitude()+
+                            "lon: " + locationResult.getLastLocation().getLongitude() + "  " +locations.size());
+                    locations.add(locationResult.getLastLocation());
+
+
+
+                }
+            }, getMainLooper());
+        }
 
     }
 }
